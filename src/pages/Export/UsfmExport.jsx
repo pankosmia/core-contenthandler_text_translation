a0400e6a10268cbdd82b873166733f014f79c24f
lmeline <loise13@hotmail.fr>
2025-10-16 10:02:00 +0200

    modification usfm book

--- src/pages/Export/UsfmExport.jsx
+++ src/pages/Export/UsfmExport.jsx
@@ -31,27 +31,8 @@ function UsfmExport({ repoSourcePath, closeFn}) {
     const {debugRef} = useContext(debugContext);
     const [selectedBooks, setSelectedBooks] = useState([]);
     const [bookCodes, setBookCodes] = useState([]);
-    const [bookNames, setBookNames] = useState([]);
+    const [bookNames, setBookNames] = useState([repoBookCode]);
     const [open,setOpen] = useState(true);
-    console.log("bc",bookCodes);
-
-    const getProjectSummaries = async () => {
-        const summariesResponse = await getJson(`/burrito/metadata/summaries`, debugRef.current);
-        if (summariesResponse.ok) {
-            const data = summariesResponse.json;
-            const keys = Object.keys(data);
-            const firstKey = keys[0];
-            const book_Codes = data[firstKey].book_codes[0];
-            setBookNames(book_Codes);
-        }
-    }
-
-    useEffect(
-        () => {
-            getProjectSummaries().then();
-        },
-        [bookCodes,bookNames]
-    );
 
     const usfmExportOneBook = async bookCode => {
         const bookUrl = `/burrito/ingredient/raw/${repoPath}?ipath=${bookCode}.usfm`;
