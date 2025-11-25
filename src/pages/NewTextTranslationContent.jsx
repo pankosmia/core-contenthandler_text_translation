89eb65509143e35f4c27621b81e6788675c6ecfc
lmeline <loise13@hotmail.fr>
2025-11-25 13:32:06 +0100

    suppression des snackbar error

--- src/pages/NewTextTranslationContent.jsx
+++ src/pages/NewTextTranslationContent.jsx
@@ -143,10 +143,6 @@ export default function NewBibleContent() {
             );
             handleCloseCreate();
         } else {
-            enqueueSnackbar(
-                `${doI18n("pages:content:content_creation_error", i18nRef.current)}: ${response.status}`,
-                { variant: "error" }
-            );
             setErrorMessage(`${doI18n("pages:content:book_creation_error", i18nRef.current)}: ${response.status
                 }`);
             setErrorDialogOpen(true);
