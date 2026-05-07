69d0c5e26e56ac92ed1d1ea2c066a9e018f0403d
RUN-Projects <82665975+RUN-Projects@users.noreply.github.com>
2026-05-07 12:45:35 -0500

    Revert "Windows needed this to build successfully"
    This reverts commit a3ed512ec1b71f44f1587b438b1794a123c40e9b.

--- src/pages/Export/testPptr/TestPptrFirefox.jsx
+++ src/pages/Export/testPptr/TestPptrFirefox.jsx
@@ -32,7 +32,7 @@ let jxlSections = {
     startOn: "recto",
     showPageNumber: true,
     jxl: "git.door43.org/BurritoTruck/fr_juxta",
-    bcvNotes: "git.door43.org/shower/eng_tn",
+    bcvNotes: "git.door43.org/unfoldingWord/en_tn",
   },
 };
 let templates = {
