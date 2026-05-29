e17579c4b743a5d08a09ae698a7686456fd92d93
lmeline <loise13@hotmail.fr>
2026-05-29 13:58:52 +0200

    add new api url

--- src/pages/Export/ZipExport.jsx
+++ src/pages/Export/ZipExport.jsx
@@ -45,7 +45,7 @@ function ZipExport() {
     const path = params.get("repoPath");
     setRepoPath(path);
     const summariesResponse = await getJson(
-      `/burrito/metadata/summary/${path}`,
+      `/api/burrito/metadata/summary/${path}`,
       debugContext.current,
     );
     if (summariesResponse.ok) {
@@ -74,7 +74,7 @@ function ZipExport() {
   };
 
   const usfmExportZipContents = async (bookCode) => {
-    const bookUrl = `/burrito/ingredient/raw/${repoPath}?ipath=${bookCode}.usfm`;
+    const bookUrl = `/api/burrito/ingredient/raw/${repoPath}?ipath=${bookCode}.usfm`;
     const bookUsfmResponse = await getText(bookUrl, debugRef.current);
     if (!bookUsfmResponse.ok) {
       enqueueSnackbar(
@@ -115,7 +115,7 @@ function ZipExport() {
   useEffect(() => {
     const doFetch = async () => {
       const versificationResponse = await getJson(
-        "/content-utils/versification/eng",
+        "/api/content-utils/versification/eng",
         debugRef.current,
       );
       if (versificationResponse.ok) {
@@ -148,7 +148,7 @@ function ZipExport() {
           backgroundPosition: "center",
           zIndex: -1,
           backgroundImage:
-            'url("/app-resources/pages/content/background_blur.png")',
+            'url("/api/app-resources/pages/content/background_blur.png")',
           backgroundRepeat: "no-repeat",
           backdropFilter: "blur(3px)",
         }}
