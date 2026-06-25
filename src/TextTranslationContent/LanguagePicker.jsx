481089599b721eae36afb304da7c73913c5a759d
lmeline <loise13@hotmail.fr>
2026-06-25 16:57:18 +0200

    replace versification's code by PanversificationPicker

--- src/TextTranslationContent/LanguagePicker.jsx
+++ src/TextTranslationContent/LanguagePicker.jsx
@@ -1,19 +1,7 @@
-import {
-  FormControl,
-  FormHelperText,
-  Grid2,
-  InputLabel,
-  MenuItem,
-  Select,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { PanLanguagePicker, i18nContext } from "pankosmia-rcl";
-import { doI18n } from "pankosmia-lib/i18n";
+import { Grid2 } from "@mui/material";
+import { PanLanguagePicker, PanVersificationPicker } from "pankosmia-rcl";
 import { getAndSetJson } from "pankosmia-lib/http";
-import { useContext, useEffect, useState } from "react";
-import ListMenuItem from "../pages/ListMenuItem";
-import sx from "../pages/Selection.styles";
+import { useEffect, useState } from "react";
 
 export default function LanguagePicker({
   setCurrentLanguage,
@@ -23,7 +11,6 @@ export default function LanguagePicker({
   setVersification,
   open,
 }) {
-  const { i18nRef } = useContext(i18nContext);
   const [versificationCodes, setVersificationCodes] = useState([]);
   console.log("versificationCode", versificationCodes);
 
@@ -45,53 +32,11 @@ export default function LanguagePicker({
         />
       </Grid2>
       <Grid2 item size={12}>
-        <Stack spacing={1}>
-          <FormControl sx={{ width: "100%" }}>
-            <InputLabel
-              id="booksVersification-label"
-              required
-              htmlFor="booksVersification"
-              sx={sx.inputLabel}
-            >
-              {doI18n(
-                "pages:core-contenthandler_text_translation:versification_scheme",
-                i18nRef.current,
-              )}
-            </InputLabel>
-            <Select
-              variant="outlined"
-              required
-              labelId="booksVersification-label"
-              name="booksVersification"
-              inputProps={{
-                id: "bookVersification",
-              }}
-              value={versification}
-              label={doI18n(
-                "pages:core-contenthandler_text_translation:versification_scheme",
-                i18nRef.current,
-              )}
-              onChange={(event) => {
-                setVersification(event.target.value);
-              }}
-              sx={sx.select}
-            >
-              {versificationCodes.map((listItem, n) => (
-                <MenuItem key={n} value={listItem} dense>
-                  <ListMenuItem
-                    listItem={`${listItem.toUpperCase()} - ${doI18n(`scripture:versifications:${listItem}`, i18nRef.current)}`}
-                  />
-                </MenuItem>
-              ))}
-            </Select>
-            <FormHelperText>
-              {doI18n(
-                `pages:core-contenthandler_text_translation:helper_versification`,
-                i18nRef.current,
-              )}
-            </FormHelperText>
-          </FormControl>
-        </Stack>
+        <PanVersificationPicker
+          versification={versification}
+          setVersification={setVersification}
+          isOpen={open}
+        />
       </Grid2>
     </Grid2>
   );
